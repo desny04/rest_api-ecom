6563258038a518f6b1bdec8f9de8c6c3c501ca1e
desny <trsadesny@gmail.com>
2026-02-18 16:06:59 +0530

    rest_api project

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAuthenticatedOrReadOnly
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework.decorators import action
+from .serializers import *
+from .models import *
+from rest_framework_simplejwt.tokens import RefreshToken
+from .permissions impoort *
+from rest_framework.permissions import SAFE_METHODS
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+
+class AuthViewSet(viewsets.ViewSet):
+    permission_classes=[AllowAny]
+    @action(detail=False,methods=['post'])
+    def register(self,request):
+        serializer=RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {'message':'User Created'},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    @action(detail=False,methods=['post'])
+    def login(self,request):
+        username=request.data.get('username')
+        password=request.data.get('password')
+
+        user=authenticate(username=username,password=password)
+        if user:
+            refresh=RefreshToken.for_user(user)
+            return Response({
+                'access':str(refresh.access_token),
+                'refresh':str(refresh),
+                'username':user.username
+            })
+        return Response({'error':'invalid'})
+    
--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
